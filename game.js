06ea55e0d5689f260d8591532597d7c27b5a88bc
Sinadin Shan <sinadinshan66@gmail.com>
2021-06-29 21:16:47 +0530

    Layout and basic functionalities added

---
+++ game.js
@@ -0,0 +1,37 @@
+
+const colors = ["red", "blue", "green", "yellow"]
+nextSequence()
+chosenColor = colors[randomNumber]
+gamePattern = Array()
+
+// alert(chosenColor)
+$("#"+chosenColor).fadeOut(100).fadeIn(100)
+gamePattern.push(chosenColor)
+playSound(chosenColor)
+
+
+$(".btn").click(function(){
+    var clickedColor = this.classList[1]
+    playSound(clickedColor)
+    animateClick(clickedColor)
+})
+
+function animateClick(color)
+{
+    setTimeout(function(){
+        $("."+color).removeClass("pressed")
+    },100)
+    $("."+color).addClass("pressed")
+}
+
+function playSound(name)
+{
+    var sound = new Audio("sounds/"+name+".mp3")
+    sound.play()
+}
+
+function nextSequence()
+{
+    randomNumber = Math.floor(Math.random()*4)
+}
+
