797f8da8fcd4601998aa27124430be61bfcf2c06
Sinadin Shan <sinadinshan66@gmail.com>
2021-06-30 21:13:44 +0530

    Added logic, game working fine

--- game.js
+++ game.js
@@ -1,20 +1,55 @@
 
 const colors = ["red", "blue", "green", "yellow"]
-nextSequence()
-chosenColor = colors[randomNumber]
-gamePattern = Array()
+var level = 0
+var i = 0
+var started = false
+gamePattern = new Array()
+userPattern = new Array()
 
+
+$(document).keypress(function(){
+    if(!started){
+        nextSequence()
+        started = true
+    }
+})
 // alert(chosenColor)
-$("#"+chosenColor).fadeOut(100).fadeIn(100)
-gamePattern.push(chosenColor)
-playSound(chosenColor)
 
 
-$(".btn").click(function(){
+
+$(".btn").click(function clicked(){
     var clickedColor = this.classList[1]
     playSound(clickedColor)
     animateClick(clickedColor)
-})
+    userPattern.push(clickedColor)
+    checkPattern()
+    })
+
+
+function checkPattern()
+{
+    if (gamePattern[i] == userPattern[i])
+    {
+        if(i == level-1)
+        {
+            // $(".btn").unbind("click")
+            setTimeout(function(){
+                nextSequence()
+            },1000)
+        }
+        else
+            i++
+    }  
+    else
+    {
+        $("h1").html("<p>Game-over</p><p>Your score: "+(level-1)+"</p>")
+        $("body").addClass("game-over")
+        setTimeout(()=>$("body").removeClass("game-over"),200)
+        level = 0
+        userPattern = []
+        gamePattern = []
+    }
+}
 
 function animateClick(color)
 {
@@ -30,6 +65,15 @@ function playSound(name)
 
 function nextSequence()
 {
+    i = 0
     randomNumber = Math.floor(Math.random()*4)
+    level++
+    $("h1").html("Level "+level)
+
+    userPattern = []
+    chosenColor = colors[randomNumber]
+    $("#"+chosenColor).fadeOut(100).fadeIn(100)
+    gamePattern.push(chosenColor)
+    playSound(chosenColor)
 }
 
